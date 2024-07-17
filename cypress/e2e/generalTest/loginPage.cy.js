7171ede21a2e946f121e483148dee8aa15bb6327
Erick Jimenez <Erick.Jimenez@coxautoinc.com>
2024-07-17 17:21:28 -0600

    Update loginPage.cy.js

--- cypress/e2e/generalTest/loginPage.cy.js
+++ cypress/e2e/generalTest/loginPage.cy.js
@@ -5,6 +5,7 @@ describe('My first Login', () => {
         cy.visit('https://www.saucedemo.com/v1/')
     })
     it('My first login',()=>{
+        cy.log("Hello culeros");
         cy.get('#user-name').type('standard_user');
         cy.get('#password').type('secret_sauce');
         cy.get('#login-button').click();
